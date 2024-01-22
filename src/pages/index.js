a04bde23c77c4b84601bb9830147e4d81e5262fb
devlak2001 <vladimirkaric2001@gmail.com>
2024-01-22 18:52:40 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,272 @@
-import * as React from "react"
+import React, { createElement, useEffect, useRef, useState } from "react";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import "./../styles/index.scss";
 
 const IndexPage = () => {
+  const gameWrapper = useRef(null);
+  const [score, setScore] = useState("0000");
+
+  useEffect(() => {
+    let start;
+    let maxPotatoes = 500;
+    const peelers = gameWrapper.current.querySelectorAll(".peelers div");
+    console.log(peelers[0]);
+    const peelersOffsetTop =
+      gameWrapper.current.querySelector(".peelers").offsetTop;
+    const peelersHeight =
+      gameWrapper.current.querySelector(".peelers").clientHeight;
+    let speed = 0.1;
+
+    document.addEventListener("gesturestart", function (e) {
+      e.preventDefault();
+    });
+
+    function generatePotatoes(elapsedTime) {
+      const potatoRow = document.createElement("div");
+      potatoRow.dataset.elapsedTime = elapsedTime.toString();
+      potatoRow.className = "potatoRow";
+      potatoRow.style.bottom = `${window.innerHeight}px`;
+
+      const numPotatoes = Math.random() < 0.5 ? 1 : 2;
+      const columns = [1, 2, 3];
+      for (let i = 0; i < numPotatoes && maxPotatoes > 0; i++) {
+        const randomIndex = Math.floor(Math.random() * columns.length);
+
+        const potato = document.createElement("img");
+        potato.src =
+          "https://devlak2001.s3.eu-central-1.amazonaws.com/potatoPeeler/potato.png";
+        potato.style.gridColumn = `${columns[randomIndex]}`;
+        potato.dataset.peelable = "false";
+        potato.dataset.column = `${columns[randomIndex]}`;
+        potato.ontouchstart = potatoOnTouchStart;
+        potatoRow.append(potato);
+
+        maxPotatoes--;
+
+        columns.splice(randomIndex, 1);
+      }
+      gameWrapper.current.append(potatoRow);
+    }
+
+    function potatoOnTouchStart(e) {
+      const currentTarget = e.currentTarget;
+      if (
+        currentTarget.getBoundingClientRect().top + currentTarget.clientHeight >
+          peelersOffsetTop &&
+        currentTarget.getBoundingClientRect().top <
+          peelersOffsetTop + peelersHeight
+      ) {
+        currentTarget.classList.add("clicked");
+        setScore((score) => (Number(score) + 10).toString().padStart(4, "0"));
+        peelers[parseInt(currentTarget.dataset.column) - 1].style.animation =
+          "none";
+        console.log(currentTarget.clientHeight);
+        peelers[parseInt(currentTarget.dataset.column) - 1].style.animation =
+          "peeling 0.4s ease";
+      }
+    }
+
+    function step(timeStamp) {
+      if (start === undefined) {
+        start = timeStamp;
+      }
+      const elapsed = timeStamp - start;
+      const potatoRows = gameWrapper.current.querySelectorAll(".potatoRow");
+
+      if (potatoRows[potatoRows.length - 1].offsetTop > 0) {
+        generatePotatoes(elapsed);
+      }
+
+      if (speed < 0.3) {
+        speed = 0.1 + elapsed / 100000;
+      }
+
+      if (potatoRows[0].offsetTop > window.innerHeight) {
+        gameWrapper.current.removeChild(potatoRows[0]);
+      }
+
+      potatoRows.forEach((el) => {
+        el.style.bottom = `${
+          window.innerHeight - (elapsed - el.dataset.elapsedTime) * speed
+        }px`;
+      });
+
+      requestAnimationFrame(step);
+    }
+
+    generatePotatoes(0);
+    requestAnimationFrame(step);
+  }, [setScore]);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    <div className="potatoGame" ref={gameWrapper}>
+      <div className="score">
+        <div
+          className="digits"
+          style={{
+            transform: `translateY(calc(-${Number(score[0])} * 11.4vw))`,
+          }}
+        >
+          <div>
+            <span>0</span>
+          </div>
+          <div>
+            <span>1</span>
+          </div>
+          <div>
+            <span>2</span>
+          </div>
+          <div>
+            <span>3</span>
+          </div>
+          <div>
+            <span>4</span>
+          </div>
+          <div>
+            <span>5</span>
+          </div>
+          <div>
+            <span>6</span>
+          </div>
+          <div>
+            <span>7</span>
+          </div>
+          <div>
+            <span>8</span>
+          </div>
+          <div>
+            <span>9</span>
+          </div>
+        </div>
+        <div
+          className="digits"
+          style={{
+            transform: `translateY(calc(-${Number(score[1])} * 11.4vw))`,
+          }}
+        >
+          <div>
+            <span>0</span>
+          </div>
+          <div>
+            <span>1</span>
+          </div>
+          <div>
+            <span>2</span>
+          </div>
+          <div>
+            <span>3</span>
+          </div>
+          <div>
+            <span>4</span>
+          </div>
+          <div>
+            <span>5</span>
+          </div>
+          <div>
+            <span>6</span>
+          </div>
+          <div>
+            <span>7</span>
+          </div>
+          <div>
+            <span>8</span>
+          </div>
+          <div>
+            <span>9</span>
+          </div>
+        </div>
+        <div
+          className="digits"
+          style={{
+            transform: `translateY(calc(-${Number(score[2])} * 11.4vw))`,
+          }}
+        >
+          <div>
+            <span>0</span>
+          </div>
+          <div>
+            <span>1</span>
+          </div>
+          <div>
+            <span>2</span>
+          </div>
+          <div>
+            <span>3</span>
+          </div>
+          <div>
+            <span>4</span>
+          </div>
+          <div>
+            <span>5</span>
+          </div>
+          <div>
+            <span>6</span>
+          </div>
+          <div>
+            <span>7</span>
+          </div>
+          <div>
+            <span>8</span>
+          </div>
+          <div>
+            <span>9</span>
+          </div>
+        </div>
+        <div
+          className="digits"
+          style={{
+            transform: `translateY(calc(-${Number(score[3])} * 11.4vw))`,
+          }}
+        >
+          <div>
+            <span>0</span>
+          </div>
+          <div>
+            <span>1</span>
+          </div>
+          <div>
+            <span>2</span>
+          </div>
+          <div>
+            <span>3</span>
+          </div>
+          <div>
+            <span>4</span>
+          </div>
+          <div>
+            <span>5</span>
+          </div>
+          <div>
+            <span>6</span>
+          </div>
+          <div>
+            <span>7</span>
+          </div>
+          <div>
+            <span>8</span>
+          </div>
+          <div>
+            <span>9</span>
+          </div>
+        </div>
+      </div>
+      <div className="peelers">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => (
+  <>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+    />
+    <title>Home Page</title>
+  </>
+);
