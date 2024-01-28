f1d51ca1e81cdf0b568e30f2bfa8957c42e10ab4
devlak2001 <vladimirkaric2001@gmail.com>
2024-01-28 01:23:27 +0100

    Updates

---
+++ cb355538-dee92fea8f493ec0e3f2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpotato_peeler=self.webpackChunkpotato_peeler||[]).push([[365],{2091:function(t,c,e){e.d(c,{Wh:function(){return h}});var a=e(3621);function h(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=cb355538-dee92fea8f493ec0e3f2.js.map
